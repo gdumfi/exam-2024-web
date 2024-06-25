b51d3cb7c0759a80ad77cd5bd751f0b602305b32
gdumfi <44841897+gdumfi@users.noreply.github.com>
2024-06-25 13:48:42 +0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = 'static/images'
-    ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'gif'}
